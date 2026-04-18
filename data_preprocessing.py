9cbd9483830b33513bce3e3236cf0ce0941207b6
Elias <eliasstalfreelancer@gmail.com>
2026-04-19 00:28:48 +0200

    added more feautre_method and changed coresponding functions for it to work

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,75 +2,119 @@ from PIL import Image
 import os 
 import pandas as pd
 from tqdm import tqdm
-from skimage.feature import hog
+from skimage.feature import hog, local_binary_pattern
 from skimage.feature import local_binary_pattern
 from skimage.color import rgb2gray
 import cv2
 from itertools import zip_longest
+import numpy as np
 
 
 
-def image_to_feature(df:pd.DataFrame,img, features):
+def image_to_feature(df:pd.DataFrame,img,full_path, features_func):
     rows = []
     
     data = {"animal":[]}
     
-    if "Cat" in img:
+    if "Cat" in full_path:
+
         animal =  1 #"Cat"
+
     else: animal = 0 #"Dog"
-    imgread = cv2.imread(img)
+
     
-    imgreadre = cv2.resize(imgread,(300,280))
+    imgreadre = cv2.resize(img,(300,280))
     gray = cv2.cvtColor(imgreadre, cv2.COLOR_BGR2GRAY)
+
     data["animal"] = animal
 
-    feature = feature(gray)
+    features = features_func(gray)
 
     for i, f in enumerate(features):
+
         data[f"f_{i}"] = f
+
     dfcopy = pd.DataFrame([data])
     df = pd.concat([df,dfcopy], ignore_index=True)
+
     return df
 
 def is_valid_image(path):
     try:
+
         with Image.open(path) as img:
             img.verify()  # checks file integrity
+
         return True
+    
     except:
+
         return False
     
-def img_to_csv_data(animal,path,loops_unitl_save = 5,list_of_feature = None):
-    print("starting")
-    nr = 0
-    try:
-        df = pd.DataFrame(pd.read_csv("data/data.csv"))
-    except:
-        df = pd.DataFrame()
-    path = path + animal
-    for file in tqdm(os.listdir(path)):
-        full_path = os.path.join(path, file)
-        if file.endswith(".jpg"):
-            if is_valid_image(full_path) :
-                if (cv2.imread(full_path)).shape[0] >= 300 and (cv2.imread(full_path)).shape[1] >= 280:
-                    for feature in list_of_feature:
-                        df = image_to_feature(df,full_path,feature[1])
-                        if loops_unitl_save == nr:
-                            df.to_csv("data/"+animal+feature[0]+"data.csv",mode="a",header=False,index=False)
-                            df = pd.DataFrame()
-                            nr = 0
-                        nr +=1
+def img_to_csv_data(animal, path, loops_unitl_save=5, list_of_feature=None):
+    folder = path + animal
 
-def exec(animals = ["Dog","Cat"],
+    for name, feature_func in list_of_feature.items():
+        print(f"{animal} - {name}")
+        
+        df = pd.DataFrame()
+        nr = 0
+
+        for file in tqdm(os.listdir(folder)):
+            full_path = os.path.join(folder, file)
+
+            if not file.endswith(".jpg"):
+                continue
+
+            if not is_valid_image(full_path):
+                continue
+
+            img = cv2.imread(full_path)
+            if img is None:
+                continue
+
+            if img.shape[0] < 300 or img.shape[1] < 280:
+                continue
+
+            df = image_to_feature(df,img,full_path, feature_func)
+
+            nr += 1
+            if nr >= loops_unitl_save:
+                df.to_csv(
+                    f"data/{animal}_{name}.csv",
+                    mode="a",
+                    header=not os.path.exists(f"data/{animal}_{name}.csv"),
+                    index=False
+                )
+                df = pd.DataFrame()
+                nr = 0
+        # after for file loop ends
+        if not df.empty:
+            df.to_csv(
+                f"data/{animal}_{name}.csv",
+                mode="a",
+                header=not os.path.exists(f"data/{animal}_{name}.csv"),
+                index=False
+            )
+
+
+def run_pipeline(animals = ["Dog","Cat"],
     path = "data/PetImages/",list_of_feature = None):
+
     for ani in animals:
         print(ani)
         img_to_csv_data(animal= ani,path=path,list_of_feature = list_of_feature)
-        df = pd.read_csv("data/"+ani+"data.csv", header=None)
-        num_features = df.shape[1] - 1  # last column = label
-        columns =  ["animal"] + [f"f_{i}" for i in range(num_features)] 
-        df.columns = columns
-        df.to_csv("data/"+ani+"data.csv", index=False)
+
+        for name in list_of_feature:
+            df = pd.read_csv(f"data/{ani}_{name}.csv")
+       
+            num_features = df.shape[1] - 1  # last column = label
+
+            columns =  ["animal"] + [f"f_{i}" for i in range(num_features)] 
+
+            df.columns = columns
+
+            df.to_csv(f"data/{ani}_{name}.csv", index=False)
 
 
 def interleave_csv(cat_path, dog_path, output_path, chunksize=50):
@@ -111,15 +155,30 @@ def interleave_csv(cat_path, dog_path, output_path, chunksize=50):
 
         first = False
 
-
-
-if __name__ == "__main__":
-    list_of_feature = {"hog" : hog(
+def extract_HOG(gray):
+    return hog(
     gray,
     pixels_per_cell=(8, 8),
     cells_per_block=(2, 2),
-    feature_vector=True),                }
-    exec()
-    interleave_csv(cat_path="data/Catdata.csv",dog_path="data/Dogdata.csv",output_path="data/shuffleddata.csv",chunksize=5000)
-    import setup_main 
+    feature_vector=True)
+
+def extract_lbp(gray):
+    lbp = local_binary_pattern(gray, P=8, R=1, method="uniform")
+    
+    hist, _ = np.histogram(
+        lbp.ravel(),
+        bins=59,
+        range=(0, 59))
+    
+    hist = hist / (hist.sum() + 1e-6)
+    return hist
+if __name__ == "__main__":
+    list_of_feature_methods = {"HOG" : extract_HOG,"LBP":extract_lbp }
+    path_list = [
+    ["data/Cat_HOG.csv", "data/Dog_HOG.csv", "data/shuffled_HOG.csv"],
+    ["data/Cat_LBP.csv", "data/Dog_LBP.csv", "data/shuffled_LBP.csv"]]
+    run_pipeline(list_of_feature=list_of_feature_methods)
+    for path in path_list:
+        interleave_csv(cat_path=path[0],dog_path=path[1],output_path=path[2],chunksize=5000)
+    #import setup_main 
     
